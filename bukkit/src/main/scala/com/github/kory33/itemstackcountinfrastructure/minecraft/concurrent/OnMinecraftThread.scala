6718c3a4fb45f4593f0226aa81940cdc3c3f6862
Kory <korygm33@gmail.com>
2021-09-22 00:44:31 +0900

    refactor: rename trait

--- bukkit/src/main/scala/com/github/kory33/itemstackcountinfrastructure/minecraft/concurrent/OnMinecraftThread.scala
+++ bukkit/src/main/scala/com/github/kory33/itemstackcountinfrastructure/minecraft/concurrent/OnMinecraftThread.scala
@@ -2,7 +2,7 @@ package com.github.kory33.itemstackcountinfrastructure.minecraft.concurrent
 
 import cats.effect.SyncIO
 
-trait OnMinecraftTick[F[_]] {
+trait OnMinecraftThread[F[_]] {
 
   /** Run the provided synchronous action on the minecraft server's main thread.
     */

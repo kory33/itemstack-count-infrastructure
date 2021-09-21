606b95fa7588bbfec49d0d6cbc418ec2df9095ec
Kory <korygm33@gmail.com>
2021-09-21 18:09:19 +0900

    feat: add stub implementation of RedisCommandQueue

---
+++ infra-redis/src/main/scala/com/github/kory33/itemstackcountinfrastructure/infra/redis/RedisCommandQueue.scala
@@ -0,0 +1,47 @@
+package com.github.kory33.itemstackcountinfrastructure.infra.redis
+
+import cats.MonadThrow
+import com.github.kory33.itemstackcountinfrastructure.core.{
+  Command,
+  CommandRecorder,
+  StorageContentMovement
+}
+import com.github.kory33.itemstackcountinfrastructure.util.BatchedQueue
+import dev.profunktor.redis4cats.effect.MkRedis
+import dev.profunktor.redis4cats.{Redis, RedisCommands}
+
+final class RedisCommandQueue[F[_]] private (
+  utf8Commands: RedisCommands[F, String, String]
+) extends BatchedQueue[F, Command] {
+
+  override def queue(elem: Command): F[Unit] =
+    elem match {
+      case Command.RecordMovement(movement) =>
+        movement.movement match {
+          case StorageContentMovement.AddedTo(loc, count) =>
+            ???
+          case StorageContentMovement.BetweenStorages(fromLoc, toLoc, count) =>
+            ???
+          case StorageContentMovement.StorageDestroyed(loc) =>
+            ???
+        }
+      case Command.SetExplicitCount(at, stackType, count) => ???
+      case Command.AbondonRecordsOn(worldName)            => ???
+    }
+
+}
+
+object RedisCommandQueue {
+
+  /** Create [[BatchedQueue]] from [[RedisCommands]].
+    *
+    * Note that the given [[RedisCommands]] should be ready to use, and if a
+    * password authentication is required, an effect on
+    * [[dev.profunktor.redis4cats.algebra.Auth]] should be performed before this
+    * [[BatchedQueue]] is ready to use.
+    */
+  def apply[F[_]](
+    utf8Commands: RedisCommands[F, String, String]
+  ): BatchedQueue[F, Command] = RedisCommandQueue[F](utf8Commands)
+
+}

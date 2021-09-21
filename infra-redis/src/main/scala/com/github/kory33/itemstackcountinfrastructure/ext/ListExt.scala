4ff82f39a0f998df2523ff029a17231af0766f76
Kory <korygm33@gmail.com>
2021-09-22 06:51:51 +0900

    feat: fully implement infra_redis module

---
+++ infra-redis/src/main/scala/com/github/kory33/itemstackcountinfrastructure/ext/ListExt.scala
@@ -0,0 +1,49 @@
+package com.github.kory33.itemstackcountinfrastructure.ext
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+import scala.reflect.TypeTest
+
+object ListExt {
+
+  extension [A](list: List[A])
+
+    /** Splits this collection into a (mapped) prefix/suffix pair with a mapping
+      * function `f`.
+      *
+      * @example
+      * {{{
+      * List(Some(1), Some(2), None, None, Some(5)) == (List(1, 2), List(None, None, Some(5))
+      * }}}
+      */
+    def spanOption[B](f: A => Option[B]): (List[B], List[A]) = {
+      val buffer = new ListBuffer[B]()
+
+      @tailrec def go(rest: List[A]): List[A] = {
+        rest match {
+          case a :: restTail =>
+            f(a) match {
+              case Some(b) =>
+                buffer.addOne(b)
+                go(restTail)
+              case None => rest
+            }
+          case Nil => Nil
+        }
+      }
+
+      val suffix = go(list)
+
+      (buffer.toList, suffix)
+    }
+
+    def spanTypeTest[B](using tt: TypeTest[A, B]): (List[B], List[A]) =
+      spanOption(tt.unapply)
+
+  /** [[List]]'s [[spanTypeTest]] method viewed as a [[cats.data.State]].
+    */
+  def spanTypeTestState[A, B](using
+    TypeTest[A, B]
+  ): cats.data.State[List[A], List[B]] =
+    cats.data.State(list => list.spanTypeTest[B].swap)
+}
--- infra-redis/src/main/scala/com/github/kory33/itemstackcountinfrastructure/infra/redis/RedisCommandQueue.scala
+++ infra-redis/src/main/scala/com/github/kory33/itemstackcountinfrastructure/infra/redis/RedisCommandQueue.scala
@@ -1,24 +1,70 @@
 package com.github.kory33.itemstackcountinfrastructure.infra.redis
 
-import cats.MonadThrow
+import cats.data.{State, StateT}
+import cats.{Applicative, Functor, Monad, MonadThrow}
 import com.github.kory33.itemstackcountinfrastructure.core.{
   Command,
-  CommandRecorder,
-  StorageContentMovement
+  CommandRecorder
 }
+import com.github.kory33.itemstackcountinfrastructure.ext.ListExt
 import com.github.kory33.itemstackcountinfrastructure.util.BatchedQueue
 import dev.profunktor.redis4cats.effect.MkRedis
 import dev.profunktor.redis4cats.{Redis, RedisCommands}
 
-final class RedisCommandQueue[F[_]] private (
+final class RedisCommandQueue[F[_]: Applicative] private (
   utf8Commands: RedisCommands[F, String, String]
 ) extends BatchedQueue[F, Command] {
 
+  import cats.implicits.given
+
+  private def queueSetCountCommands(
+    commands: List[Command.SetExplicitCount]
+  ): F[Unit] = {
+    val grouped = commands.groupBy(_.at.worldName)
+
+    grouped.toList.traverse { case (worldName, commands) =>
+      utf8Commands.hmSet(
+        worldName,
+        commands.map { command =>
+          s"${command.stackType}/${command.at.x}/${command.at.y}/${command.at.z}" -> command.count.toString
+        }.toMap
+      )
+    }.void
+  }
+
+  private def queueAbondonRecordsCommand(
+    command: Command.AbondonRecordsOn
+  ): F[Unit] = utf8Commands.del(command.worldName).void
+
   override def queue(elem: Command): F[Unit] =
     elem match {
-      case Command.SetExplicitCount(at, stackType, count) => ???
-      case Command.AbondonRecordsOn(worldName)            => ???
+      case elem: Command.SetExplicitCount =>
+        queueSetCountCommands(List(elem))
+      case elem: Command.AbondonRecordsOn =>
+        queueAbondonRecordsCommand(elem)
     }
+
+  /** Queue [[Command]]s in a way such that the number of commands issued to the
+    * backend Redis is minimized as much as possible. For example, consecutive
+    * [[SetExplicitCount]] is combined into a single invocation of
+    * [[queueSetCountCommands]]
+    */
+  override def queueList(elems: List[Command])(using F: Monad[F]): F[Unit] = {
+    val effect = F.iterateWhileM(elems) { list =>
+      val (remainingList, effectsToRun) = List(
+        ListExt
+          .spanTypeTestState[Command, Command.SetExplicitCount]
+          .map(queueSetCountCommands),
+        ListExt
+          .spanTypeTestState[Command, Command.AbondonRecordsOn]
+          .map(_.traverse(queueAbondonRecordsCommand).void)
+      ).sequence.run(list).value
+
+      effectsToRun.sequence.as(remainingList)
+    }(_.nonEmpty)
+
+    effect.void
+  }
 
 }
 
@@ -31,7 +77,7 @@ object RedisCommandQueue {
     * [[dev.profunktor.redis4cats.algebra.Auth]] should be performed before this
     * [[BatchedQueue]] is ready to use.
     */
-  def apply[F[_]](
+  def apply[F[_]: Functor](
     utf8Commands: RedisCommands[F, String, String]
   ): BatchedQueue[F, Command] = RedisCommandQueue[F](utf8Commands)
 

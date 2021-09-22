7d7bbe9c6d4670a9d9e62daa70b6c1d35b332de2
Kory <korygm33@gmail.com>
2021-09-23 07:49:09 +0900

    fix: do not use inventory's snapshot
    because that causes some puzzling NullPointerException

--- bukkit/src/main/scala/com/github/kory33/itemstackcountinfrastructure/bukkit/inspection/algebra/InspectBukkitWorld.scala
+++ bukkit/src/main/scala/com/github/kory33/itemstackcountinfrastructure/bukkit/inspection/algebra/InspectBukkitWorld.scala
@@ -3,11 +3,11 @@ package com.github.kory33.itemstackcountinfrastructure.bukkit.inspection.algebra
 import cats.Functor
 import cats.effect.SyncIO
 import com.github.kory33.itemstackcountinfrastructure.core
-import com.github.kory33.itemstackcountinfrastructure.core._
+import com.github.kory33.itemstackcountinfrastructure.core.*
 import com.github.kory33.itemstackcountinfrastructure.core.algebra.InspectStorages
 import com.github.kory33.itemstackcountinfrastructure.minecraft.concurrent.OnMinecraftThread
 import org.bukkit.inventory.{Inventory, ItemStack}
-import org.bukkit.{Bukkit, Location}
+import org.bukkit.{Bukkit, Location, Material}
 
 object InspectBukkitWorld {
 
@@ -17,52 +17,40 @@ object InspectBukkitWorld {
         val worldGrouped: Map[String, List[(Int, Int, Int)]] =
           targets.targets.toList.groupMap(_.worldName)(l => (l.x, l.y, l.z))
 
-        val inspectOnMainThread: F[List[(StorageLocation, Option[Inventory])]] =
+        val inspectOnMainThread: F[List[(StorageLocation, LocationInspectionResult)]] =
           OnMinecraftThread[F].run(SyncIO {
             for {
               (worldName, locs) <- worldGrouped.toList
               world <- Option.apply(Bukkit.getWorld(worldName)).toList
               (x, y, z) <- locs
             } yield {
-              val inventorySnapshot = {
+              import scala.jdk.CollectionConverters.given
+
+              val result =
                 world.getBlockAt(x, y, z).getState match {
                   case state: org.bukkit.block.Container =>
-                    Some(state.getSnapshotInventory())
-                  case _ => None
+                    LocationInspectionResult.Found {
+                      state
+                        .getInventory
+                        .asScala
+                        .toList
+                        .filter(s => s != null && s.getType != Material.AIR)
+                        .groupMap(_.getType.name)(_.getAmount)
+                        .map {
+                          case (name, amounts) =>
+                            (ItemStackTypeName.apply(name), amounts.sum)
+                        }
+                        .toMap
+                    }
+                  case _ =>
+                    LocationInspectionResult.NoContainerFound
                 }
-              }
 
-              (StorageLocation(worldName, x, y, z), inventorySnapshot)
+              (StorageLocation(worldName, x, y, z), result)
             }
           })
 
-        Functor[F].map(inspectOnMainThread) { list =>
-          core.InspectionResult {
-            val resultList: Seq[(StorageLocation, LocationInspectionResult)] = for {
-              (location, inventoryOption) <- list
-            } yield {
-              inventoryOption match {
-                case Some(inventorySnapshot) =>
-                  val inventoryCollection: Iterable[ItemStack] =
-                    new scala.jdk.CollectionConverters.IterableHasAsScala(
-                      inventorySnapshot
-                    ).asScala
-
-                  val itemAmounts: ItemAmounts =
-                    inventoryCollection.groupMap(_.getType.name)(_.getAmount).map {
-                      case (name, amounts) =>
-                        (ItemStackTypeName.apply(name), amounts.sum)
-                    }
-
-                  (location, LocationInspectionResult.Found(itemAmounts))
-                case None =>
-                  (location, LocationInspectionResult.NoContainerFound)
-              }
-            }
-
-            resultList.toMap
-          }
-        }
+        Functor[F].map(inspectOnMainThread)(list => core.InspectionResult(list.toMap))
       }
     }
 
